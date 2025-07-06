aa8cdb212c5ef12cda4c1bd15b6e2b917e9ebb35
Joydip Koley <joydipkoley12@gmail.com>
2025-07-06 12:24:31 +0530

    Deleted Unwanted Folders

--- Set_74/script.js
+++
@@ -1,8 +0,0 @@
-let btn1 = document.getElementById("btn")
-//event listening functions 
-btn1.addEventListener("click", ()=>(
-    document.querySelector(".box").innerHTML="i am now clicked."
-))
-document.addEventListener("keydown", (e)=>(
-    console.log(e.key,e.keyCode)
-))
